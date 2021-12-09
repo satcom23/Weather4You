c7413eabd586dea4955ea8a8887aa3c71d062f42
Jimmy Ojeda <jimmyojeda94@gmail.com>
2021-12-08 21:30:47 -0800

    api connection

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import {useState} from 'react';
+import { useState } from "react";
 import "./index.css";
-import axios from 'axios';
-
-
+import axios from "axios";
 
 function App() {
-
   const api = {
     key: "029ce530124449bad57430e958df0d80",
     base: "http://api.openweathermap.org/data/2.5/",
   };
 
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
 
-  const search = evt => {
+  const search = (evt) => {
     if (evt.key === "Enter") {
-        axios.get(`${api.base}weather?q=${query}&appid=${api.key}`)
-          .then(res => res.json())
-          .then(result => {
-            setWeather(result);
-            setQuery('');
-            console.log(result)
-          });
+      axios
+        .get(`${api.base}weather?q=${query}&appid=${api.key}`)
+        .then((res) => res.data)
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
     }
-  }
+  };
 
   const dateBuilder = (dates) => {
     let months = [
@@ -59,33 +57,52 @@ function App() {
     return `${day} ${date} ${month} ${year}`;
   };
 
+  const temperatureConverter = (valTemp) => {
+    valTemp = parseFloat(valTemp);
+    valTemp = (valTemp - 273.15) * 1.8 + 32;
+    return Math.round(valTemp);
+  };
+
   return (
-    <div className="App">
+    <div
+      className={
+        (typeof weather.main != "undefined")
+          ? ((temperatureConverter(weather.main.temp) < 60)
+            ? "App cold"
+            : "App")
+          : "App"
+      }
+    >
       <main>
         <div className="input">
           <input
             type="text"
             className="input-field"
             placeholder="Enter your location..."
-            onChange={event => setQuery(event.target.value)}
+            onChange={(event) => setQuery(event.target.value)}
             value={query}
             onKeyPress={search}
           />
         </div>
-        {(typeof weather.main != "undefined") ? (
-        <>
-        <div className="location-box">
-          <div className="location">{weather.name}, {weather.sys.country}</div>
-          <div className="date">{dateBuilder(new Date())}</div>
-        </div>
-        <div className="weather-box">
-          <div className="temp">
-            40ºF
-          </div>
-          <div className="weather">Sunny</div>
-        </div>
-        </>
-        ) : ('')}
+        {typeof weather.main != "undefined" ? (
+          <>
+            {console.log(weather)}
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">
+                {temperatureConverter(weather.main.temp)}ºF
+              </div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </>
+        ) : (
+          ""
+        )}
       </main>
     </div>
   );
