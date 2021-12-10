07271ebe91cffe53a8e67c3f2d2ba4a0e6fb64d8
Jimmy Ojeda <jimmyojeda94@gmail.com>
2021-12-10 06:54:29 -0800

    final touch ups

--- src/App.js
+++ src/App.js
@@ -1,108 +1,176 @@
-import { useState } from "react";
+import { useEffect,useState } from "react";
 import "./index.css";
-import axios from "axios";
+import {useDispatch, useSelector} from 'react-redux';
+import { fetchWeatherAction } from "./redux/slices/weatherSlices";
 
 function App() {
-  const api = {
-    key: "029ce530124449bad57430e958df0d80",
-    base: "http://api.openweathermap.org/data/2.5/",
-  };
+  const [city, setCity] = useState("");
 
-  const [query, setQuery] = useState("");
-  const [weather, setWeather] = useState({});
+  //dispatch action
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(fetchWeatherAction(''));
+  }, []);
 
-  const search = (evt) => {
-    if (evt.key === "Enter") {
-      axios
-        .get(`${api.base}weather?q=${query}&appid=${api.key}`)
-        .then((res) => res.data)
-        .then((result) => {
-          setWeather(result);
-          setQuery("");
-          console.log(result);
-        });
-    }
-  };
+  //select state from store
+  const state = useSelector((state)=> state);
+  const {weather, loading, error} = state;
 
-  const dateBuilder = (dates) => {
-    let months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-
-    let day = days[dates.getDay()];
-    let date = dates.getDate();
-    let month = months[dates.getMonth()];
-    let year = dates.getFullYear();
-
-    return `${day} ${date} ${month} ${year}`;
-  };
-
+  //helper functions
   const temperatureConverter = (valTemp) => {
     valTemp = parseFloat(valTemp);
     valTemp = (valTemp - 273.15) * 1.8 + 32;
     return Math.round(valTemp);
   };
 
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      dispatch(fetchWeatherAction(city))
+      
+    }
+
+  }
+
+  let timeStamp = ""
+  let shortStamp = ""
+  let currentStamp = ""
+  let day = ""
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+  const updateTime = () => {
+    let currentTime = new Date();
+    let hours = currentTime.getUTCHours();
+    let minutes = currentTime.getUTCMinutes();
+    let seconds = currentTime.getUTCSeconds();
+    let year = currentTime.getUTCFullYear();
+    let month = currentTime.getUTCMonth();
+    let date = currentTime.getUTCDate();
+    if (minutes < 10){
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10){
+        seconds = "0" + seconds;
+    }
+     timeStamp = hours + ":" + minutes + ":" + seconds + " ";
+    if(hours > 11){
+        timeStamp+="PM";
+    } else {
+        timeStamp+="AM"
+    }
+    timeStamp = `${year}-${month}-${date} ` + timeStamp
+    return timeStamp
+  }
+  const currentTime = () => {
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let prefix = "AM";
+    if (minutes < 10){
+        minutes = "0" + minutes;
+    }
+    if(hours>12){
+      hours = hours - 12;
+      prefix = "PM"
+    }
+     currentStamp = hours + ":" + minutes + " " + prefix;
+    return currentStamp
+  }
+  currentTime()
+  updateTime()
+  let forecast = weather?.list;
+// JSX starts here
   return (
-    <div
-      className={
-        (typeof weather.main != "undefined")
-          ? ((temperatureConverter(weather.main.temp) < 60)
-            ? "App cold"
-            : "App")
-          : "App"
-      }
-    >
+    // conditional for dynamic background
+    <div className={`App ${temperatureConverter(weather?.list[0].main.temp) <= 50 ? "cold" : ""}`}>
       <main>
         <div className="input">
           <input
             type="text"
             className="input-field"
             placeholder="Enter your location..."
-            onChange={(event) => setQuery(event.target.value)}
-            value={query}
-            onKeyPress={search}
+            value={city}
+            onChange={e=> setCity(e.target.value)}
+            onKeyDown={handleKeyDown}
           />
+          <button 
+            type = "button"
+            className="submit-button"
+            onClick={()=> dispatch(fetchWeatherAction(city))}
+            />
         </div>
-        {typeof weather.main != "undefined" ? (
+        {!weather?.list[0].main.temp ? <h1>See the upcoming forecast </h1> : <h1>The Date is {timeStamp} UTC</h1>}
+        {loading ? (
+        <h1>Processing...</h1>
+        ) : weather?.list[0].main.temp ? (
           <>
-            {console.log(weather)}
             <div className="location-box">
               <div className="location">
-                {weather.name}, {weather.sys.country}
+                {weather?.city.name}, {weather?.city.country}
               </div>
-              <div className="date">{dateBuilder(new Date())}</div>
             </div>
             <div className="weather-box">
               <div className="temp">
-                {temperatureConverter(weather.main.temp)}ºF
+                {temperatureConverter(weather?.list[0].main.temp)}ºF
               </div>
-              <div className="weather">{weather.weather[0].main}</div>
+              <div className="weather-icon">
+                <img 
+                src={`https://openweathermap.org/img/wn/${weather?.list[0].weather[0].icon}@4x.png`} 
+                alt = {`${weather?.list[0].weather.main}`} 
+                />
+              </div>
+              <div className="weather">It looks like {weather?.list[0].weather[0].main}</div>
+              <ul>
+                  {forecast.map((item,index)=>{
+                    let unix_timestamp = item?.dt
+                    let date = new Date(unix_timestamp * 1000)
+
+                    const shortTime = () => {
+                      let shortTime = date;
+                      let hours = shortTime.getUTCHours();
+                      let minutes = shortTime.getUTCMinutes();
+                      
+                      let prefix = "AM";
+                      if (minutes < 10){
+                          minutes = "0" + minutes;
+                      }
+                      if(hours>12){
+                        hours = hours - 12;
+                        prefix = "PM"
+                      }
+                       shortStamp = hours + ":" + minutes + " " + prefix;
+                      return shortStamp
+                    }
+                    const dayPlacer = () => {
+                      let dayStamp = date                      
+                      day = dayStamp.getUTCDay();
+                      return day
+                    }
+                    dayPlacer()
+                    shortTime()
+                    if (index < 12) {
+                      return (
+                        <>
+                          <li key={index}>
+                            {shortStamp}
+                            <br/>
+                            {temperatureConverter(item.main.temp)}ºF
+                          <img 
+                            src={`https://openweathermap.org/img/wn/${weather?.list[0].weather[0].icon}@2x.png`} 
+                            alt = {`${weather?.list[0].weather.main}`} 
+                            />
+                            <br/>
+                            {days[day]}
+                          </li>
+                        </>)}
+                        return null
+                  })}
+              </ul>
             </div>
-          </>
-        ) : (
-          ""
-        )}
+          </> 
+        ) : error?.message === "Nothing to geocode" ? (<></>) 
+        : error ? (<h1>{error?.message}</h1>) : <></>
+      }
+      
       </main>
     </div>
   );
